a6fd7e2b1ce464eac43b65050df526d565b3dd3a
Askerov Javid <askerovlab@gmail.com>
2017-03-13 19:37:35 +0400

    Webpack almost configured, all other files minor non-important sample changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
+import '../css/style.css';
+
 (function() {
-
+  document.getElementsByTagName('h1')[0].innerHTML = 'Changed text';
   console.log('Hey hey'); // eslint-disable-line no-console
-
-});
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+
+process.noDeprecation = true;
 
 module.exports = {
   
-  entry: "src/js/index.js", 
-  devtool: "source-map", 
+  entry: './src/js/index.js', 
+  devtool: 'source-map', 
   context: __dirname,
-  target: "web", 
-  stats: "errors-only",
+  target: 'web', 
+  stats: 'errors-only',
+  watch: true,
+
 
   output: {
-    path: path.resolve(__dirname, "src"), 
-    filename: "bundle.js", 
-    publicPath: "/", 
+    path: path.resolve(__dirname, './dist'), 
+    filename: 'js/[name].[chunkHash].js', 
+    publicPath: '/', 
   },
 
   module: {
@@ -19,27 +27,55 @@ module.exports = {
       {
         test: /\.jsx?$/,
         include: [
-          path.resolve(__dirname, "src")
+          path.resolve(__dirname, 'src')
         ],
         exclude: [
-          path.resolve(__dirname, "node_modules")
+          path.resolve(__dirname, 'node_modules')
         ],
 
-        loader: "babel-loader",
+        loader: 'babel-loader',
 
         options: {
-          presets: ["es2015"]
+          presets: ['es2015']
         },
       },
+      {
+        test: /\.css$/, 
+        use: ExtractTextPlugin.extract({
+                use: 'css-loader'
+           })
+      }
     ]
   },
 
-  devServer: {
-    contentBase: path.join(__dirname, "src"),
-    compress: true,
-    port: 9000
-  }
+  plugins: [
+    new WebpackMd5Hash(),
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
 
+  devServer: {
+    contentBase: path.join(__dirname, "dist/"),
+    compress: true,
+    port: 8080
+}
 
 }
 
