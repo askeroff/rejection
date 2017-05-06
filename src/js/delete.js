01df894be561b91b873b33de99ec1c6c41815e2f
Askerov Javid <askerovlab@gmail.com>
2017-05-06 19:39:27 +0400

    fixed: delete didn't work properly

--- src/js/delete.js
+++ src/js/delete.js
@@ -1,8 +1,7 @@
 import renderData from './view';
 
-const state = JSON.parse(localStorage.getItem('rejectionData'));
-
 export default function deleteFromArray(arr, i) {
+  const state = JSON.parse(localStorage.getItem('rejectionData'));
   state.data = arr.filter((item, index) => index !== i);
   const newObj = Object.assign({}, state);
   localStorage.setItem('rejectionData', JSON.stringify(newObj));
